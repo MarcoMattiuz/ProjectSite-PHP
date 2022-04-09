43a363b001c172d1918a4bf68f6149af2ee81765
lorenzolashkiba <lorenzoboiko@gmail.com>
2022-04-09 17:02:46 +0200

    finished hamburger menu

--- scripts/toggleNav.js
+++ scripts/toggleNav.js
@@ -4,11 +4,12 @@ const blur = document.querySelector(".blur");
 function toggleNav(){
 
     if(hamburger.style.visibility!="visible"){
-
+        hamburger.classList.add("ham-show");
         hamburger.style.visibility="visible";
         blur.style.visibility="visible";
     }else{
         hamburger.style.visibility="hidden";
+        hamburger.classList.remove("ham-show");
         blur.style.visibility="hidden";
     }
   
